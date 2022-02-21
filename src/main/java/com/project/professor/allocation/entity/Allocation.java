de1aafa70f69be7f211820c49c90ae71306ac08d
Fernando Vilela <rfavilela@gmail.com>
2022-02-20 22:28:45 -0300

    Relacionamento Allocation

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -3,12 +3,15 @@ package com.project.professor.allocation.entity;
 import java.time.DayOfWeek;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -18,12 +21,32 @@ public class Allocation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private DayOfWeek day;
+	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
 	private Date start;
+	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
 	private Date end;
+	
+	@Column(name = "course_id", nullable = false)
+	private Long courseId;
+	@Column(name = "professor_id",nullable = false)
+	private Long professorId;
+	
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "professor_id", updatable = false, insertable = false, nullable = false)
+	private Professor prof;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "course_id", updatable = false, insertable = false, nullable = false)
+	private Course curso;
+	
+	
+	
 
 	public Long getId() {
 		return id;
@@ -73,7 +96,22 @@ public class Allocation {
 		this.professorId = professorId;
 	}
 
-	private Long courseId;
-	private Long professorId;
+	public Professor getProf() {
+		return prof;
+	}
+
+	public void setProf(Professor prof) {
+		this.prof = prof;
+	}
+
+	public Course getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Course curso) {
+		this.curso = curso;
+	}
+
+
 
 }
--- src/main/java/com/project/professor/allocation/entity/Department.java
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -1,5 +1,6 @@
 package com.project.professor.allocation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,7 +12,9 @@ public class Department {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(unique = true, nullable = false)
 	private String name;
+	
 	public Long getId() {
 		return id;
 	}
